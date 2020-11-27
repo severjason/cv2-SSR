db6ff9b77dd249d3864a2dfeaf3a8a80112317e8
severjason <ssever2010@gmail.com>
2020-11-27 10:37:16 +0200

    added all data

--- next.config.js
+++ next.config.js
@@ -2,13 +2,15 @@ const { locales, defaultLocale } = require('./i18n.json');
 const withImages = require('next-images');
 const withPWA = require('next-pwa');
 
-module.exports = withPWA(withImages({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-  },
-  i18n: {
-    locales,
-    defaultLocale,
-  },
-}));
+module.exports = withPWA(
+  withImages({
+    pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      dest: 'public',
+    },
+    i18n: {
+      locales,
+      defaultLocale,
+    },
+  })
+);
